cfff57dd9c3f26377964e9a49bbcaea28fdd7d8f
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-06-14 20:18:46 +0800

    Upload BillVending.java

--- src/com/billvending/BillVending.java
+++ src/com/billvending/BillVending.java
@@ -1,1 +1,29 @@
 
+import java.util.Scanner;
+
+import com.billvnd.BillVnd;
+public class BillVending {
+    public static void main(String[] args) {
+        Scanner scmain = new Scanner(System.in);
+        int choice = 0;
+        BillVnd vnd = new BillVnd();
+        while(choice!=3){
+            System.out.println("Press 1 for coin maintenance, 2 for purchasing, and 3 to exit");
+            choice = scmain.nextInt();
+            scmain.nextLine();
+            if(choice==1){
+                vnd.billMaintenance();
+            }
+            else if(choice==2){
+                vnd.purchaseItem();
+            }
+            else if(choice==3){
+                System.out.println("You are exiting the vending machine. Have a nice day!");
+            }
+            else{
+                System.out.println("Invalid choice. Please try again");
+            }
+        }
+        scmain.close();
+    }
+}
