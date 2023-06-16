4be7bb6c60ff0f2520a9a77d6ea3ffb9f6239904
Trispers <136597522+Trispers@users.noreply.github.com>
2023-06-16 16:27:36 +0800

    Update Rgitem.java

--- src/com/rgitem/Rgitem.java
+++ src/com/rgitem/Rgitem.java
@@ -1,74 +1,77 @@
 package com.rgitem;
 
-public class Rgitem {
+public class RgItem{
     
-    private int[] intQty;
-    private int[] intPrice;
-    private int[] intCalorie;
-    private String[] strDesc;
-    private String[] strName;
+    private int intQty;
+    private int intPrice;
+    private int intCalorie;
+    private String strDesc;
+    private String strName;
 
-    public void setName(String strName, int intCounter){
-        this.strName[intCounter] = strName;
+    //Set
+    public void setName(String strName){
+        this.strName = strName;
     }
 
-    public void setDesc(String strDesc, int intCounter){
-        this.strDesc[intCounter] = strDesc;
+    public void setDesc(String strDesc){
+        this.strDesc = strDesc;
     }
 
-    public void setCalorie(int intCalorie, int intCounter){
-        this.intCalorie[intCounter] = intCalorie;
+    public void setCalorie(int intCalorie){
+        this.intCalorie = intCalorie;
     }
 
-    public void setPrice(int intPrice, int intCounter){
-        this.intPrice[intCounter] = intPrice;
+    public void setPrice(int intPrice){
+        this.intPrice = intPrice;
     }
 
-    public void setQty(int intQty, int intCounter){
-        this.intCalorie[intCounter] = intQty;
+    public void setQty(int intCalorie){
+        this.intCalorie = intCalorie;
     }
 
-    public String getName(int intCounter){
-        return strName[intCounter];
+    //Get
+    public String getName(){
+        return strName;
     }
 
-    public String getDesc(int intCounter){
-        return strDesc[intCounter];
+    public String getDesc(){
+        return strDesc;
     }
 
-    public int getCalorie(int intCounter){
-        return intCalorie[intCounter];
+    public int getCalorie(){
+        return intCalorie;
     }
 
-    public int getPrice(int intCounter){
-        return intPrice[intCounter];
+    public int getPrice(){
+        return intPrice;
     }
 
-    public int getQty(int intCounter){
-        return intCalorie[intCounter];
+    public int getQty(){
+        return intCalorie;
     }
 
-    public void changeQuantitySub(int intSub, int intCounter){
-        this.intQty[intCounter] = this.intQty[intCounter] - intSub;
+    //Quantity Add/Sub
+    public void changeQuantitySub(int intQty){
+        this.intQty = this.intQty - intQty;
     }
 
-    public void changeQuantityAdd(int intAdd, int intCounter){
-        this.intQty[intCounter] = this.intQty[intCounter] + intAdd;
+    public void changeQuantityAdd(int intQty){
+        this.intQty = this.intQty + intQty;
     }
 
-    public void deleteCake(int intCounter){
-        this.intQty[intCounter] = 0;
-        this.intPrice[intCounter] = 0;
-        this.intCalorie[intCounter] = 0;
-        this.strDesc[intCounter] = " ";
-        this.strName[intCounter] = " ";
+    public void deleteCake(){
+        intQty = 0;
+        intPrice = 0;
+        intCalorie = 0;
+        strDesc = " ";
+        strName = " ";
     }
 
-    public void displayCake(int intCounter){
-        System.out.println("Cake Name: " + this.strName[intCounter]);
-        System.out.println("Description: " + this.strDesc[intCounter]);
-        System.out.println("Calorie Count: " + this.intCalorie[intCounter]);
-        System.out.println("Price: " + this.intPrice[intCounter]);
-        System.out.println("Available Quantity: " + this.intQty[intCounter]);
+    public void displayCake(){
+        System.out.println("Cake Name: " + strName);
+        System.out.println("Description: " + strDesc);
+        System.out.println("Calorie Count: " + intCalorie);
+        System.out.println("Price: " + intPrice);
+        System.out.println("Available Quantity: " + intQty);
     }
 }
