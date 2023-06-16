4a29f4fdb008513fbe34b586a045828ca4e901d5
Trispers <136597522+Trispers@users.noreply.github.com>
2023-06-16 21:53:34 +0800

    Update Rgitem.java

--- src/com/rgitem/Rgitem.java
+++ src/com/rgitem/Rgitem.java
@@ -1,5 +1,7 @@
 package com.rgitem;
-
+ /*
+ * The class Rg item
+ */ 
 public class RgItem{
     
     private int intQty;
@@ -7,64 +9,130 @@ public class RgItem{
     private int intCalorie;
     private String strDesc;
     private String strName;
+/*
+ * Sets the name
+ *
+ * @param strName the str name. 
+ */
+    public void setName(String strName){ 
 
-    public void setName(String strName){
         this.strName = strName;
     }
+/*
+ * Sets the description
+ *
+ * @param strDesc the str description. 
+ */
+    public void setDesc(String strDesc){ 
 
-    public void setDesc(String strDesc){
         this.strDesc = strDesc;
     }
+/*
+ * Sets the calorie
+ *
+ * @param intCalorie the int calorie. 
+ */
+    public void setCalorie(int intCalorie){ 
 
-    public void setCalorie(int intCalorie){
         this.intCalorie = intCalorie;
     }
+/*
+ * Sets the price
+ *
+ * @param intPrice the int price. 
+ */
+    public void setPrice(int intPrice){ 
 
-    public void setPrice(int intPrice){
         this.intPrice = intPrice;
     }
+/*
+ * Sets the quantity
+ *
+ * @param intQty the int quantity. 
+ */
+    public void setQty(int intQty){ 
 
-    public void setQty(int intQty){
         this.intQty = intQty;
     }
+/*
+ * Gets the name
+ *
+ * @return the name
+ */
+    public String getName(){ 
 
-    public String getName(){
         return strName;
     }
+/*
+ * Gets the description
+ *
+ * @return the description
+ */
+    public String getDesc(){ 
 
-    public String getDesc(){
         return strDesc;
     }
+/*
+ * Gets the calorie
+ *
+ * @return the calorie
+ */
+    public int getCalorie(){ 
 
-    public int getCalorie(){
         return intCalorie;
     }
+/*
+ * Gets the price
+ *
+ * @return the price
+ */
+    public int getPrice(){ 
 
-    public int getPrice(){
         return intPrice;
     }
+/*
+ * Gets the quantity
+ *
+ * @return the quantity
+ */
+    public int getQty(){ 
 
-    public int getQty(){
         return intQty;
     }
+/*
+ * Changes the quantity through subtraction
+ *
+ * @param intQty the int quantity to be subtracted. 
+ */
+    public void changeQtySub(int intQty){ 
 
-    public void changeQtySub(int intQty){
         this.intQty = this.intQty - intQty;
     }
+/*
+ * Changes the quantity through addition
+ *
+ * @param intQty the int quantity to be added.
+ */
+    public void changeQtyAdd(int intQty){ 
 
-    public void changeQtyAdd(int intQty){
         this.intQty = this.intQty + intQty;
     }
+/*
+ * Deletes cake
+ */
+    public void deleteCake(){ 
 
-    public void deleteCake(){
         intQty = 0;
         intPrice = 0;
         intCalorie = 0;
         strDesc = " ";
         strName = " ";
     }
+/*
+ * Display cake
+ */
+    public void displayCake(){ 
 
-    public void displayCake(){
         System.out.println("Cake Name: " + strName);
         System.out.println("Description: " + strDesc);
         System.out.println("Calorie Count: " + intCalorie);
