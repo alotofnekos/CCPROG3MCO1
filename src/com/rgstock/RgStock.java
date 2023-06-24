cde83a1162836617111126ff70ff4d9c43da1663
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-06-24 12:21:48 +0800

    First "complete" + connected build

--- src/com/rgstock/RgStock.java
+++ src/com/rgstock/RgStock.java
@@ -7,44 +7,69 @@ import com.rgitem.RgItem;
  * The class Rg stock
  */ 
 public class RgStock{
+    private RgItem[] Item;
+    private Scanner intMenu = new Scanner(System.in);
+    private Scanner strEdit = new Scanner(System.in);
+/**
+ * Initialize
+ *
+ * @param Item  The Item Array. 
+ */
+    public RgStock(){ 
+        int intCounter = CountCakes(Item);
+        Item = new RgItem[20];
+        Item[intCounter].rgItem("Apple Crumble", 0, 160, 156, "Treat yourself with our irresistible apple crumble! Indulge in layers of tender apples, delicately spiced with cinnamon, and crowned with a mouthwatering crumbly crust.");
+        Item[intCounter+1].rgItem("Black Forest Cake", 0, 140, 282,"Savor the taste of the classic Black Forest Cake, where the tangy sweetness of cherries perfectly complements the rich chocolate layers, all brought together with light and fluffy cream.");
+        Item[intCounter+2].rgItem("Premium Chocolate Cake", 0, 190, 389, "Treat yourself with this moist, rich, and bittersweet Chocolate cake, adorned with a lucious dark chocolate ganache.");        
+        Item[intCounter+3].rgItem("Strawberry Shortcake", 0, 250, 247, "Indulge in the freshness of Benguet's finest strawberries with our Strawberry Bliss Short Cake. Enjoy the perfect blend of delicate sponge cake, heavenly cream, and juicy strawberries, sourced directly from the strawberry capital of the Philippines.");
+        Item[intCounter+4].rgItem("Lemon Blueberry Cake", 0, 240, 244, "Enjoy the vibrant combination of zesty lemon-infused cake, bursting blueberries, and the smooth cream cheese frosting that creates a perfect balance of flavors with this Lemon Blueberry cake.");
+        Item[intCounter+5].rgItem("Chocolate Mousse Cake", 0, 220, 349, "Revel in joy with a decadent dessert featuring layers of moist chocolate cake and creamy chocolate mousse, altogether forming a rich and indulgent treat.");
+        Item[intCounter+6].rgItem("Butterscotch Cake", 0, 280, 379, "A delectable dessert with a moist and fluffy base infused with the rich and caramel-like flavor of butterscotch, topped with creamy butterscotch frosting and caramel sauce.");
+        Item[intCounter+7].rgItem("Mocha Delight Cake", 0, 210, 308, "Enjoy a heavenly dessert featuring layers of moist chocolate cake infused with the bold flavors of coffee and espresso, complemented by a velvety mocha frosting for a delightful and indulgent treat.");
+        Item[intCounter+8].rgItem("Mango Surprise Cake", 0, 230, 228, "A tropical delight that combines layers of moist mango-infused cake with a luscious mango filling, providing a delightful surprise of fruity sweetness in every bite.");
+        Item[intCounter+9].rgItem("Carrot Cake", 0, 180, 266, "Treat yourself with a moist and flavorful dessert made with grated carrots, warm spices, and a rich cream cheese frosting, offering a delightful combination of sweet and spiced flavors.");
+    }
+    public void setStockDefaults(){
+        for(int i = 0;i<Item.length;i++){
+            Item[i].setQty(10);
+        }
+    }
+/**
+ * Displays the menu.
+ * 
+ */
+    public void displayMenu(){
+        for(int i = 0;i<Item.length;i++){
+            System.out.println("Press " + i + " for this cake:");
+            Item[i].displayCake();
+        }        
+    }
 /**
  * Main
- *
- * @param args the args. 
+ * 
  */
     public void stockMenu(){ 
 
         int intLoop;
         int intChosen;
-        int intBoolean = 0;
-        RgItem[] Item = new RgItem[100];
-        Scanner intMenu = new Scanner(System.in);
-        Scanner strEdit = new Scanner(System.in);
-        
-        for(intLoop=0;intLoop<100;intLoop++){
-            Item[intLoop] = new RgItem();
-            Item[intLoop].setName(" ");
-        }
 
-        while(intBoolean == 0){
-            DisplayStockMenu();
+        while(intChosen != 3){
+            System.out.println("[1] Edit Cake");
+            System.out.println("[2] Add Cake");
+            System.out.println("[3] Return");
+            System.out.println("------------------------");
+            System.out.print("Enter Choice: ");
             intChosen = intMenu.nextInt();
             switch(intChosen){
                 case 1:
-                    System.out.println("Initialized 10 Base Cakes...");
-                    System.out.println("------------------------");
-                    Initialize(Item);
+                    editMenu(intMenu, strEdit);
                     break;
                 case 2:
-                    editMenu(Item, intMenu, strEdit);
+                    AddCake(intMenu, strEdit);
                     break;
                 case 3:
-                    AddCake(Item, intMenu, strEdit);
-                    break;
-                case 4:
                     System.out.println("Returning...");
                     System.out.println("------------------------");
-                    intBoolean = 1;
                     break;
                 default:
                     System.out.println("Invalid Input...");
@@ -63,7 +88,7 @@ public class RgStock{
  * @param intMenu  the Integer Scanner. 
  * @param strEdit  the String Scanner. 
  */
-    public void editMenu(RgItem[] Item, Scanner intMenu, Scanner strEdit){ 
+    public void editMenu(){ 
 
 
         int intChosen;
@@ -82,7 +107,16 @@ public class RgStock{
                 
             while(intBooleanEdit == 0){
                 Item[intChosen].displayCake();
-                DisplayEditMenu();
+                System.out.println("[1] Edit Name");
+                System.out.println("[2] Edit Description");
+                System.out.println("[3] Edit Calorie");
+                System.out.println("[4] Edit Price");
+                System.out.println("[5] Add Inventory");
+                System.out.println("[6] Subtract Inventory");
+                System.out.println("[7] Delete Cake");
+                System.out.println("[8] Return");
+                System.out.println("------------------------");
+                System.out.print("Enter Choice: ");
                 intEditMenu = intMenu.nextInt();
 
                 switch(intEditMenu) {
@@ -155,43 +189,42 @@ public class RgStock{
  * @param intMenu  The Integer Scanner. 
  * @param strEdit  the String Scanner.
  */
-    public void AddCake(RgItem[] Item, Scanner intMenu, Scanner strEdit){ 
-
-
-        int intCalorie;
-        int intPrice;
-        int intQty;
+    public void AddCake(){ 
+        int intValue;
         int intCounter = CountCakes(Item);
         String strName;
         String strDesc;
 
         System.out.print("Enter Name: ");
         strName = strEdit.nextLine();
+        Item[intCounter].setName(strName);
         System.out.println("------------------------");
 
         System.out.print("Enter Description: ");
         strDesc = strEdit.nextLine();
+        Item[intCounter].setDesc(strDesc);
         System.out.println("------------------------");
         
         System.out.print("Enter Calorie Count: ");
-        intCalorie = intMenu.nextInt();
+        intValue = intMenu.nextInt();
+        Item[intCounter].setCalorie(intValue);
         System.out.println("------------------------");
         
         System.out.print("Enter Price Count: ");
-        intPrice = intMenu.nextInt();
+        intValue = intMenu.nextInt();
+        Item[intCounter].setPrice(intValue);
         System.out.println("------------------------");
         
         System.out.print("Enter Quantity Count [Max 10]: ");
-        intQty = intMenu.nextInt();
-        if(intQty > 10){
-            intQty = 0;
+        intValue = intMenu.nextInt();
+        if(intValue > 10){
+            intValue = 0;
             System.out.println("Invalid Quantity. Setting Quantity to 0.");
         }
+        Item[intCounter].setQty(intValue);
         System.out.println("------------------------");
         System.out.println("Cake Registration Done");
         System.out.println("------------------------");
-
-        Item[intCounter].rgItem(strName, intQty, intPrice, intCalorie, strDesc);
     }
 /**
  * Count cakes
@@ -199,77 +232,49 @@ public class RgStock{
  * @param Item  The Item Array. 
  * @return int  Number of Cakes in Item.
  */
-    public int CountCakes(RgItem[] Item){ 
-
-
+    public int CountCakes(){ 
         int intLoop;
         int intCounter = 0;
 
-        for(intLoop=0;intLoop<100;intLoop++){
-            if(Item[intLoop].getName() != " "){
+        for(intLoop=0;intLoop<20;intLoop++){
+            if(Item[intLoop].getName() != null){
                 intCounter = intCounter + 1;
             }
         }
-
         return intCounter;
     }
 /**
- *
- * Display stock menu
- *
- */
-    public void DisplayStockMenu(){ 
-
-        System.out.println("[1] Initialize Standard 10 Cakes");
-        System.out.println("[2] Edit Cake");
-        System.out.println("[3] Add Cake");
-        System.out.println("[4] Return");
-        System.out.println("------------------------");
-        System.out.print("Enter Choice: ");
-    }
-/*
- * Display edit menu
- */
-    public void DisplayEditMenu(){ 
-
-        System.out.println("[1] Edit Name");
-        System.out.println("[2] Edit Description");
-        System.out.println("[3] Edit Calorie");
-        System.out.println("[4] Edit Price");
-        System.out.println("[5] Add Inventory");
-        System.out.println("[6] Subtract Inventory");
-        System.out.println("[7] Delete Cake");
-        System.out.println("[8] Return");
-        System.out.println("------------------------");
-        System.out.print("Enter Choice: ");
-    }
-/**
- * Initialize
+ * Transfer a cake to another when its bought
  *
  * @param Item  The Item Array. 
+ * @return int  Number of Cakes in Item.
  */
-    public void Initialize(RgItem[] Item){ 
+    public void transferCake(RgStock rgStock, int cakeIndex) {
+        rgStock.Item[cakeIndex].changeQtyAdd(1);
+        this.Item[cakeIndex].changeQtySub(1);
+    }
 
-        int intCounter = CountCakes(Item);
+/**
+ * Check if the cake at Item[cakeIndex] is valid (exists and is not null)
+ *  @param cakeIndex  The index of the cake in the Item array.
+ *  @return boolean  Returns true if the cake is valid, false otherwise.
+ */
+    public boolean validItem(int cakeIndex) {
+        return (cakeIndex >= 0 && cakeIndex < Item.length && Item[cakeIndex] != null);
+    }
 
-        Item[intCounter].rgItem("Apple Crumble", 0, 160, 156, "Treat yourself with our irresistible apple crumble! Indulge in layers of tender apples, delicately spiced with cinnamon, and crowned with a mouthwatering crumbly crust.");
-
-        Item[intCounter+1].rgItem("Black Forest Cake", 0, 140, 282,"Savor the taste of the classic Black Forest Cake, where the tangy sweetness of cherries perfectly complements the rich chocolate layers, all brought together with light and fluffy cream.");
-
-        Item[intCounter+2].rgItem("Premium Chocolate Cake", 0, 190, 389, "Treat yourself with this moist, rich, and bittersweet Chocolate cake, adorned with a lucious dark chocolate ganache.");
-        
-        Item[intCounter+3].rgItem("Strawberry Shortcake", 0, 250, 247, "Indulge in the freshness of Benguet's finest strawberries with our Strawberry Bliss Short Cake. Enjoy the perfect blend of delicate sponge cake, heavenly cream, and juicy strawberries, sourced directly from the strawberry capital of the Philippines.");
-
-        Item[intCounter+4].rgItem("Lemon Blueberry Cake", 0, 240, 244, "Enjoy the vibrant combination of zesty lemon-infused cake, bursting blueberries, and the smooth cream cheese frosting that creates a perfect balance of flavors with this Lemon Blueberry cake.");
-
-        Item[intCounter+5].rgItem("Chocolate Mousse Cake", 0, 220, 349, "Revel in joy with a decadent dessert featuring layers of moist chocolate cake and creamy chocolate mousse, altogether forming a rich and indulgent treat.");
-
-        Item[intCounter+6].rgItem("Butterscotch Cake", 0, 280, 379, "A delectable dessert with a moist and fluffy base infused with the rich and caramel-like flavor of butterscotch, topped with creamy butterscotch frosting and caramel sauce.");
-
-        Item[intCounter+7].rgItem("Mocha Delight Cake", 0, 210, 308, "Enjoy a heavenly dessert featuring layers of moist chocolate cake infused with the bold flavors of coffee and espresso, complemented by a velvety mocha frosting for a delightful and indulgent treat.");
-
-        Item[intCounter+8].rgItem("Mango Surprise Cake", 0, 230, 228, "A tropical delight that combines layers of moist mango-infused cake with a luscious mango filling, providing a delightful surprise of fruity sweetness in every bite.");
-
-        Item[intCounter+9].rgItem("Carrot Cake", 0, 180, 266, "Treat yourself with a moist and flavorful dessert made with grated carrots, warm spices, and a rich cream cheese frosting, offering a delightful combination of sweet and spiced flavors.");
+/**
+ * Display the receipt showing the cake name, quantity, and total price for each cake.
+ */
+    public void getReceipt() {
+        System.out.println("Name\t\tQty\tPrice");
+        for (int i = 0; i < Item.length; i++) {
+            if (validItem(i)) {
+                RgItem cake = Item[i];
+                int quantity = cake.getQty();
+                int totalPrice = cake.getPrice() * quantity;
+                System.out.println(cake.getName() + "\t\t" + quantity + "\t" + "P" + totalPrice);
+            }
+        }
     }
 }
