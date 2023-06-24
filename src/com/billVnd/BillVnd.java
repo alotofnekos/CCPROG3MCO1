b8783107f1d862a50803dc4da09966e183ca78ac
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-06-24 19:48:31 +0800

    added a way to bypass buying items if no items to be bought

--- src/com/billVnd/BillVnd.java
+++ src/com/billVnd/BillVnd.java
@@ -70,64 +70,70 @@ public class BillVnd {
         boolean canGiveChange;
         int cakeIndex=-1;
         boolean isItemValid = false;
-        System.out.println("Total amount of bills in Vending machine: " + vndStock.getTotalAmount());
-        if(vndStock.getTotalAmount()==0){
-            System.out.println("The Vending Machine does not have money to process change right now");
-            canGiveChange = false;
+        boolean hasCakeInStock = vndItemStock.hasCakeInStock();
+        if(hasCakeInStock==false){
+            System.out.println("Cant buy items, no items in stock.");
         }
         else{
-            System.out.println("The Vending Machine can process change right now");
-            canGiveChange = true;
-        }
-        vndItemStock.displayMenu();
-        int[] userBills = new int[userWallet.getBills().length];
-        for (int i = 0; i < userBills.length; i++) {
-            int billValue = userWallet.getBills()[i].getValue();
-            System.out.print("Enter the number of " + billValue + " Peso bills to pay:");
-            userBills[i] = sc.nextInt();
-            userWallet.addBill(new Bill(userBills[i], billValue));
-            if(i-1>userBills.length){
-                sc.nextLine();
+            System.out.println("Total amount of bills in Vending machine: " + vndStock.getTotalAmount());
+            if(vndStock.getTotalAmount()==0){
+                System.out.println("The Vending Machine does not have money to process change right now");
+                canGiveChange = false;
             }
-        }
-        System.out.println("Total amount inserted:" + userWallet.getTotalAmount());
-        while(isItemValid==false){
-            
-            System.out.print("Enter the number of the item you want to buy");
-            cakeIndex = sc.nextInt();
-            sc.nextLine();
-            isItemValid = vndItemStock.validItem(cakeIndex);
-            if(isItemValid==false){
-                System.out.println("Invalid item. Please try again");
-            }
-        }
-        int price = vndItemStock.getCakePrice(cakeIndex);
-        if(canGiveChange == false){
-            if(userWallet.getTotalAmount()==price){
-                vndStock.pay(userWallet.getTotalAmount(),price);
-                userWallet.transferBills(vndStock);
-                vndItemStock.transferCake(vndItemSold,cakeIndex);
-            }
-            else if(userWallet.getTotalAmount()>price){
-                System.out.println("Please pay exact amount. No change available");
-                userWallet.clearWallet();
-            }
-        }
-        else{
-            if(userWallet.getTotalAmount()<price){
-                System.out.println("Insufficient funds. Please add more bills");
-                userWallet.clearWallet();
-            }
-            else if ((vndStock.pay(userWallet.getTotalAmount(),price)==true)&&(userWallet.getTotalAmount()>price)) {
-                userWallet.transferBills(vndProfit);
-                vndItemStock.transferCake(vndItemSold,cakeIndex);
-            } 
-
             else{
-                System.out.println("Please pay exact amount. No change available");
-                userWallet.clearWallet();
+                System.out.println("The Vending Machine can process change right now");
+                canGiveChange = true;
             }
-        }   
+            vndItemStock.displayMenu();
+            int[] userBills = new int[userWallet.getBills().length];
+            for (int i = 0; i < userBills.length; i++) {
+                int billValue = userWallet.getBills()[i].getValue();
+                System.out.print("Enter the number of " + billValue + " Peso bills to pay:");
+                userBills[i] = sc.nextInt();
+                userWallet.addBill(new Bill(userBills[i], billValue));
+                if(i-1>userBills.length){
+                    sc.nextLine();
+                }
+            }
+            System.out.println("Total amount inserted:" + userWallet.getTotalAmount());
+            while(isItemValid==false){
+                
+                System.out.print("Enter the number of the item you want to buy");
+                cakeIndex = sc.nextInt();
+                sc.nextLine();
+                isItemValid = vndItemStock.validItem(cakeIndex);
+                if(isItemValid==false){
+                    System.out.println("Invalid item. Please try again");
+                }
+            }
+            int price = vndItemStock.getCakePrice(cakeIndex);
+            if(canGiveChange == false){
+                if(userWallet.getTotalAmount()==price){
+                    vndStock.pay(userWallet.getTotalAmount(),price);
+                    userWallet.transferBills(vndStock);
+                    vndItemStock.transferCake(vndItemSold,cakeIndex);
+                }
+                else if(userWallet.getTotalAmount()>price){
+                    System.out.println("Please pay exact amount. No change available");
+                    userWallet.clearWallet();
+                }
+            }
+            else{
+                if(userWallet.getTotalAmount()<price){
+                    System.out.println("Insufficient funds. Please add more bills");
+                    userWallet.clearWallet();
+                }
+                else if ((vndStock.pay(userWallet.getTotalAmount(),price)==true)&&(userWallet.getTotalAmount()>price)) {
+                    userWallet.transferBills(vndProfit);
+                    vndItemStock.transferCake(vndItemSold,cakeIndex);
+                } 
+
+                else{
+                    System.out.println("Please pay exact amount. No change available");
+                    userWallet.clearWallet();
+                }
+            }  
+        } 
         //sc.close();     
     }
     public void receipt(){
