f5bcb9359940349fec3acaa30c360c1e6502b2b8
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-06-30 12:03:32 +0800

    Made a few things more verbose

--- src/com/billVending/BillVending.java
+++ src/com/billVending/BillVending.java
@@ -13,12 +13,27 @@ public class BillVending {
         int cakeIndex;
         int subChoice = 0;
         int choice = 0;
+        int initialize =0;
         int integer;
         int loop;
         String string;
         int[] vndBills = {0, 0, 0, 0, 0, 0};
+        while (initialize != 2 && initialize != 1) {
+                    System.out.print("Press 1 to initialize a regular vending machine, press 2 to exit: ");
+                    initialize = scmain.nextInt();
+                    scmain.nextLine();
 
-        while(choice!=3){
+                    if (initialize == 2) {
+                        System.out.println("Exiting program. Have a nice day!");
+                    } else if (initialize == 1) {
+                        System.out.println("Initializing a regular cake vending machine!");
+                        System.out.println("...");
+                        System.out.println("Initialized a regular cake vending machine!");
+                    } else {
+                        System.out.println("Invalid input.");
+                    }
+        }
+        while(choice!=3&&initialize==1){
             subChoice=0;
             System.out.println("Press 1 to access maintenance features, 2 to test vending features, and 3 to exit");
             choice = scmain.nextInt();
@@ -29,8 +44,9 @@ public class BillVending {
                     subChoice=scmain.nextInt();
                     scmain.nextLine();
                     if(subChoice==1){
-                        System.out.println("Enter bills in order of 10, 20, 50, 100, 200, 500");
+                        
                         for(loop = 0; loop < 6; loop++){
+                            System.out.print("Enter the number of "+ vnd.getBillAmt(loop)+ " Peso bills to add: ");
                             vndBills[loop] = scmain.nextInt();
                         }
                         vnd.billMaintenance(vndBills);
@@ -38,15 +54,16 @@ public class BillVending {
                     else if(subChoice==2){
                         System.out.print("What do you want to edit?\nName [1]\nDescription [2]\nCalorie [3]\nPrice [4]\nAdd Stock [5]\nSubtract Stock [6]\nEnter Choice: ");
                         loop = scmain.nextInt();
+                        vnd.displayMenu();
                         System.out.print("Enter cake number you want this edit to occur\nEnter: ");
                         cakeIndex = scmain.nextInt();
                         if(loop > 2){
-                            System.out.print("Enter New Value: ");
+                            System.out.print("Enter Value: ");
                             integer = scmain.nextInt();
                             vnd.editCake(loop, cakeIndex, integer);
                         }
                         else{
-                            System.out.print("Enter New Value: ");
+                            System.out.print("Enter Value: ");
                             string = str.nextLine();
                             vnd.editCake(loop, cakeIndex, string);
                         }
@@ -67,24 +84,34 @@ public class BillVending {
                         System.out.println("Invalid choice. Please try again");
                     }
                 }
+                
             }
             else if(choice==2){
                 do{
-                    System.out.println("Purchase Item\nEnter bills in order of 10, 20, 50, 100, 200, 500");
-                    for(loop = 0; loop < 6; loop++){
-                        vndBills[loop] = scmain.nextInt();
-                    }
-                    System.out.print("Enter cake number you want to buy\nEnter: ");
-                    cakeIndex = scmain.nextInt();
+                    System.out.println("Purchase Item");
+                    if(vnd.hasStock()==true){
+                        vnd.displayMenu();
+                        for(loop = 0; loop < 6; loop++){
+                            System.out.print("Enter the number of "+ vnd.getBillAmt(loop)+ " Peso bills to add: ");
+                            vndBills[loop] = scmain.nextInt();
+                        }
+                        System.out.print("Enter cake number you want to buy\nEnter: ");
+                        cakeIndex = scmain.nextInt();
 
-                    vnd.purchaseItem(vndBills, cakeIndex);
-                    while(subChoice!=2){
-                        System.out.println("Buy another item? Press 1 if Yes, 2 if No");
-                        subChoice=scmain.nextInt();
-                        scmain.nextLine();
-                        
+                        vnd.purchaseItem(vndBills, cakeIndex);
+                        while(subChoice!=2){
+                            System.out.println("Buy another item? Press 1 if Yes, 2 if No");
+                            subChoice=scmain.nextInt();
+                            scmain.nextLine();
+                            
+                        }
+                        System.out.println("Returning to the main menu...");
                     }
-                    System.out.println("Returning to the main menu...");
+                    else{
+                        System.out.println("Can't buy items, no items in stock.");
+                        subChoice=2;
+                    }
+
                 }while(subChoice!=2);
                 
             }
