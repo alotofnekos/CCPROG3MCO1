852da4834d6514e254cecc85fb47eece80e7f267
Trispers <136597522+Trispers@users.noreply.github.com>
2023-06-30 11:12:30 +0800

    Update BillVending.java

--- src/com/billVending/BillVending.java
+++ src/com/billVending/BillVending.java
@@ -67,7 +67,6 @@ public class BillVending {
                         System.out.println("Invalid choice. Please try again");
                     }
                 }
-                
             }
             else if(choice==2){
                 do{
