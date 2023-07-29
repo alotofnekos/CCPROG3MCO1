f7b31929c2b5584cd0773e9bb658b7c33ea610ff
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-07-29 19:00:47 +0800

    getcakedetails

--- MCO1/RgVnd.java
+++ MCO1/RgVnd.java
@@ -23,6 +23,10 @@ public class RgVnd {
     public void clearUserWallet(){
         userWallet.clearWallet();        
     }
+
+    public String getCakeDetails(int cakeIndex){
+        return vndItemStock.displayDetails(cakeIndex);
+    }
 /**
  * Adds bills to the vending machine's stock
  * @param vndBills the array of bills corresponding to the number of [10, 20, 50, 100, 200, 500] peso bills
