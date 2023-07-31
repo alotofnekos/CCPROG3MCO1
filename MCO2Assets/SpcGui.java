f7d23fbe861a90f49906ea3fb4e9f95e0ccda60f
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-07-31 16:01:55 +0800

    Update SpcGui.java - not fully working yet

--- MCO2Assets/SpcGui.java
+++ MCO2Assets/SpcGui.java
@@ -1,24 +1,28 @@
-import java.awt.Color;
+import javax.swing.*;
+import javax.swing.text.NumberFormatter;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.text.NumberFormat;
+
+import javax.imageio.ImageIO;
 import java.util.ArrayList;
 import java.util.Arrays;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
 
 public class SpcGui extends Gui {
     private JButton[] spcButtons;
     private JPanel[] spcVendingP;
-    private int xSpcPos;
-    private int ySpcPos;
+    private JButton selectAddOn;
+    private int xSpcPos=20;
+    private int ySpcPos=270;
     private JTextArea[] spcItemInfoTextArea;
     private SpcVnd vnd;
     private ArrayList<String> spcitemNames = new ArrayList<>();
     private ArrayList<String> spcitemImageFileNames = new ArrayList<>();
+    private ArrayList<String> selectedItems = new ArrayList<>();
     public SpcGui() {
         super();
         vnd = new SpcVnd();
@@ -32,18 +36,22 @@ public class SpcGui extends Gui {
             spcitemNames.add("AddOn#"+(i+1));
             i++;
         }
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\BDay.Topper.NoBg.png");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\MDay.Topper.NoBg.png");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\FDay.Topper.NoBg.png");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\candle.NoBg.jpg");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\Flower.Topper.NoBg.jpg");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\Heart.Topper.NoBg.jpg");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\Apple.Topping.NoBg.jpg");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\choco.NoBg.jpg");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\IceCream.Topping.NoBg.jpg");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\birthdaycake.jpg");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\alamodefruit.jpg");
-        spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\alamodechoc.jpg");
+        i=0;
+        while(vnd.validSpcItem(i)){
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\BDay.Topper.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\MDay.Topper.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\FDay.Topper.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\candle.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\Flower.Topper.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\Heart.Topper.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\Apple.Topping.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\choco.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\IceCream.Topping.NoBg.png");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\birthdaycake.jpg");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\alamodefruit.jpg");
+            spcitemImageFileNames.add("C:\\Users\\Angel\\Downloads\\CCPROG3MCO1\\MCO2Assets\\alamodechoc.jpg");
+            i++; 
+        }
         Arrays.fill(spcButtons, new JButton());
         Arrays.fill(spcVendingP, new JPanel());
     }
@@ -59,32 +67,134 @@ public class SpcGui extends Gui {
         String itemName;
         String imageFileName;
         ImageIcon imageIcon;
-        for (int itemIndex = 0; itemIndex < 20; itemIndex++) {
-            if (itemIndex < itemNames.size() && itemIndex < itemImageFileNames.size()) {
-                itemName = itemNames.get(itemIndex);
-                imageFileName = itemImageFileNames.get(itemIndex);
-                imageIcon = loadImageIcon(imageFileName,50,50);
+        for (int itemIndex = 0; itemIndex < 16; itemIndex++) {
+            if (itemIndex < spcitemImageFileNames.size()) {
+                itemName = spcitemNames.get(itemIndex);
+                imageFileName = spcitemImageFileNames.get(itemIndex);
+                imageIcon = loadImageIcon(imageFileName, 50, 50);
             } else {
                 itemName = "Default";
-                imageFileName = "C:\\\\\\\\Users\\\\\\\\Angel\\\\\\\\Downloads\\\\\\\\CCPROG3MCO1\\\\\\\\MCO2Assets\\\\\\\\genericcake.jpg"; // Replace with the correct path to your default image
-                imageIcon = loadImageIcon(imageFileName,50,50);
+                imageFileName = "C:\\\\\\\\Users\\\\\\\\Angel\\\\\\\\Downloads\\\\\\\\CCPROG3MCO1\\\\\\\\MCO2Assets\\\\\\\\genericcaketopper.jpg"; 
+                imageIcon = loadImageIcon(imageFileName, 50, 50);
             }
-            setupItemButton(itemIndex, xPos, yPos, itemName, imageIcon);
-            if (itemIndex < itemImageFileNames.size()) {
-                setupItemDetailPanel(itemIndex);
+            setupSpcItemButton(itemIndex, xSpcPos, ySpcPos, itemName, imageIcon);
+            if (itemIndex < spcitemImageFileNames.size()) {
+                setupSpcItemDetailPanel(itemIndex);
             }
-            xPos += 70;
-            if (itemIndex<=20 && (itemIndex + 1) % 10 == 0) {
-                xPos = 20;
-                yPos += 120;
+            xSpcPos += 70;
+            if (itemIndex == 7) {
+                xSpcPos = 20;
+                ySpcPos += 120;
             }
-            if(itemIndex==19){
-                System.out.println("xPos: "+ xPos+ " yPos: " + yPos);
+        }
+    }    
+
+    public void setupSpcItemButton(int itemIndex, int x, int y, String itemName, ImageIcon imageIcon) {
+        spcButtons[itemIndex] = new JButton();
+        spcButtons[itemIndex].setBounds(x, y, 70, 75);
+        spcButtons[itemIndex].addActionListener(this);
+        spcButtons[itemIndex].setVisible(false);
+
+        spcVendingP[itemIndex] = new JPanel(); // Initialize the spcVendingP array element here
+        spcVendingP[itemIndex].setBounds(725, 30, 350, 250);
+        spcVendingP[itemIndex].setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        spcVendingP[itemIndex].setLayout(new BorderLayout());
+
+        JLabel itemImageLabel = new JLabel(imageIcon);
+        JLabel textLabel = new JLabel(itemName);
+        itemImageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        spcVendingP[itemIndex].add(itemImageLabel, BorderLayout.CENTER);
+        spcVendingP[itemIndex].add(textLabel, BorderLayout.SOUTH); // Add the textLabel to the SOUTH
+        spcButtons[itemIndex].setMargin(new Insets(5, 5, 5, 5));
+        spcButtons[itemIndex].setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0)); // Set the layout to FlowLayout
+        spcButtons[itemIndex].add(itemImageLabel, BorderLayout.NORTH);
+        spcButtons[itemIndex].add(textLabel, BorderLayout.SOUTH);
+        spcVendingP[itemIndex].setVisible(false);
+        add(spcVendingP[itemIndex]);
+        add(spcButtons[itemIndex]);
+    }
+
+    public void setupSpcItemDetailPanel(int itemIndex) {
+        spcVendingP[itemIndex] = new JPanel();
+        spcVendingP[itemIndex].setBounds(725, 30, 350, 250);
+        spcVendingP[itemIndex].setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        spcVendingP[itemIndex].setLayout(new BorderLayout());
+    
+        // Load the image and create a JLabel as before
+        ImageIcon itemImageIcon = loadImageIcon(spcitemImageFileNames.get(itemIndex), 125, 125);
+        JLabel itemImageLabel = new JLabel(itemImageIcon);
+        itemImageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        spcVendingP[itemIndex].add(itemImageLabel, BorderLayout.CENTER);
+    
+        // Create a JTextArea to display the item information
+        spcItemInfoTextArea[itemIndex] = new JTextArea();
+        spcItemInfoTextArea[itemIndex].setEditable(false);
+        spcItemInfoTextArea[itemIndex].setLineWrap(true);
+        spcItemInfoTextArea[itemIndex].setWrapStyleWord(true);
+    
+        // Get the item information using getItemInfoString() method
+        String itemInfo = vnd.getItemDetails(itemIndex);
+    
+        // Set the item information as the text of the JTextArea
+        spcItemInfoTextArea[itemIndex].setText(itemInfo);
+    
+        // Add the JTextArea to the SOUTH of the spcVendingP panel
+        spcVendingP[itemIndex].add(spcItemInfoTextArea[itemIndex], BorderLayout.SOUTH);
+    
+        spcVendingP[itemIndex].setVisible(false);
+        add(spcVendingP[itemIndex]);
+    }
+    public void addOn(){
+        selectAddOn.setBounds(600, 295, 125, 50);
+        selectAddOn.addActionListener(this);
+        selectAddOn.setVisible(false);
+        add(selectAddOn);
+    }
+    @Override
+    public void Display() {
+        super.Display();
+        initializeSpcItemButtons();
+        addOn();
+    }
+    @Override
+    public void visible() {
+        super.visible();
+        if (menuInt == 0) {
+            selectAddOn.setVisible(true);
+            for (int i = 0; i < spcButtons.length; i++) {;
+                spcButtons[i].setVisible(true);
+                spcButtons[i].setBackground(Color.LIGHT_GRAY);
             }
-            /*if(itemIndex > 20&& (itemIndex-19) % 8 == 0){
-                xPos=20;
-                yPos+=120;
-            }*/
+        }
+    }
+    @Override
+    public void inVisible() {
+        super.inVisible();
+        selectAddOn.setVisible(false);
+
+        for(int i = 0; i < spcButtons.length; i++) {
+            spcVendingP[i].setVisible(false);
+            spcButtons[i].setVisible(false);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent click) {
+        super.actionPerformed(click);
+        for (int i = 0; i < spcButtons.length; i++) {
+            if (click.getSource() == spcButtons[i] && menuInt == 0) {
+                for(int j=0;j<spcButtons.length;j++){
+                    spcVendingP[j].setVisible(false);
+                    spcButtons[j].setBackground(Color.LIGHT_GRAY);
+                }
+                for(int j=0;j<vendingP.length;j++){
+                    vendingP[j].setVisible(false);
+                }
+                spcVendingP[i].setVisible(true);
+                totalPrice 
+                amountTotal.setText( + "Pesos");
+                spcButtons[i].setBackground(Color.GREEN);
+            }    
         }
     }
 }
