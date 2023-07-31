36000c2a964c09c56777b16d79f6700a211bbde3
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-07-31 19:58:30 +0800

    Update SpcStock.java - added FlavorText

--- MCO1/SpcStock.java
+++ MCO1/SpcStock.java
@@ -13,30 +13,30 @@ public class SpcStock {
     public SpcStock() {
         spitem = new SpcItem[16];
         // Initialize default special items
-        spitem[0] = new SpcItem("Happy Birthday Dedication", 0, 20, 74, "A special option for your cake, allowing you to greet your special person (or yourself) a Happy Birthday with this edible topper", "dedication");
-        spitem[1] = new SpcItem("Happy Mothers Day Dedication", 0, 20, 74, "A special option for your cake, allowing you to greet your special person (or yourself) a Happy Mothers Day with this edible topper", "dedication");
-        spitem[2] = new SpcItem("Happy Fathers Day Dedication", 0, 20, 74, "A special option for your cake, allowing you to greet your special person (or yourself) a Happy Fathers Day with this ", "dedication");
-        spitem[3] = new SpcItem("Cake Candle", 0, 10, 0, "A special option for your cake, allowing you to bring a warm glow to your cake. Dont forget to make a wish before blowing!", "candle");
-        spitem[4] = new SpcItem("Candy Flowers", 0, 40, 55, "A special option for your cake, allowing you to bring more color with these vibrant candied flowers. Not suitable for Crumbling cakes.", "topper");
+        spitem[0] = new SpcItem("Happy Birthday Dedication", 0, 20, 74, "A special option for your cake, allowing you to greet your special person (or yourself) a Happy Birthday with this edible topper", "dedication", "Wishing the reciever a Happy Birthday as we place this topper on your order!");
+        spitem[1] = new SpcItem("Happy Mothers Day Dedication", 0, 20, 74, "A special option for your cake, allowing you to greet your special person (or yourself) a Happy Mothers Day with this edible topper", "dedication","Wishing a Happy Mother's day to the reciever, which is the sweetest mom in the world, as we place this topper on your order!");
+        spitem[2] = new SpcItem("Happy Fathers Day Dedication", 0, 20, 74, "A special option for your cake, allowing you to greet your special person (or yourself) a Happy Fathers Day with this ", "dedication","Cheers to the best father in the world as we place this Happy Father's day topper!");
+        spitem[3] = new SpcItem("Cake Candle", 0, 10, 0, "A special option for your cake, allowing you to bring a warm glow to your cake. Dont forget to make a wish before blowing!", "candle", "Let this candle's glow symbolize the warmth of love and celebration.\nWishing for more Celebrations to come!");
+        spitem[4] = new SpcItem("Candy Flowers", 0, 40, 55, "A special option for your cake, allowing you to bring more color with these vibrant candied flowers. Not suitable for Crumbling cakes.", "topper","Sprinkling colorful blooms of joy to sweeten your day!");
         spitem[4].addIncompatibleCake("Angel's Apple Crumble");
-        spitem[5] = new SpcItem("Candy Hearts", 0, 40, 60, "A special option for your cake, allowing you to bring more love with these cute candied hearts. Not suitable for Crumbling cakes.", "topper");
+        spitem[5] = new SpcItem("Candy Hearts", 0, 40, 60, "A special option for your cake, allowing you to bring more love with these cute candied hearts. Not suitable for Crumbling cakes.", "topper","Sending love and sweet thoughts with these candy hearts!");
         spitem[5].addIncompatibleCake("Angel's Apple Crumble");
-        spitem[6] = new SpcItem("Fruits", 0, 30, 24, "Add natural sweetness to your dessert with fruits! Tristan's Carrot Cake and Black Forest Cake cannot add fruit to the top of the cake.", "fruit");
+        spitem[6] = new SpcItem("Fruits", 0, 30, 24, "Add natural sweetness to your dessert with fruits! Tristan's Carrot Cake and Black Forest Cake cannot add fruit to the top of the cake.", "fruit","Adding fresh and juicy fruits to add a delightful twist to your cake!");
         spitem[6].addIncompatibleCake("Tristan's Carrot Cake");
         spitem[6].addIncompatibleCake("Black Forest Cake");
-        spitem[7] = new SpcItem("Chocolate", 0, 40, 70, "Add a hint of bitter-sweet to your day with these chocolate toppings. Unavailable to Chocolate Mousse Cake,Black Forest Cake, and Premium Chocolate Cake as these are already chocolate cakes.", "chocolate");
+        spitem[7] = new SpcItem("Chocolate", 0, 40, 70, "Add a hint of bitter-sweet to your day with these chocolate toppings. Unavailable to Chocolate Mousse Cake,Black Forest Cake, and Premium Chocolate Cake as these are already chocolate cakes.", "chocolate","Adding in rich, dark chocolate to transform this cake into the ultimate treat!");
         spitem[7].addIncompatibleCake("Chocolate Mousse Cake");
         spitem[7].addIncompatibleCake("Black Forest Cake");
         spitem[7].addIncompatibleCake("Premium Chocolate Cake");
-        spitem[8] = new SpcItem("Vanilla Ice Cream", 0, 30, 66, "Beat the heat by adding a scoop of fragrant and rich vanilla ice cream on your cake!Unavailable to Tristan's Carrot Cake, Black Forest Cake, Strawberry Cake, Chocolate Mousse Cake, and Lemon Blueberry Cake due to these cakes already have a delicate topping. ", "ice cream");
+        spitem[8] = new SpcItem("Vanilla Ice Cream", 0, 30, 66, "Beat the heat by adding a scoop of fragrant and rich vanilla ice cream on your cake!Unavailable to Tristan's Carrot Cake, Black Forest Cake, Strawberry Cake, Chocolate Mousse Cake, and Lemon Blueberry Cake due to these cakes already have a delicate topping. ", "ice cream","Adding in a scoop of creamy vanilla ice cream to complement your cake perfectly.\n Consume immediately!");
         spitem[8].addIncompatibleCake("Tristan's Carrot Cake");
         spitem[8].addIncompatibleCake("Black Forest Cake");
         spitem[8].addIncompatibleCake("Strawberry Cake");
         spitem[8].addIncompatibleCake("Chocolate Mousse Cake");
         spitem[8].addIncompatibleCake("Lemon Blueberry Cake");
-        spitem[9] = new SpcItem("Upgrade to B-Day Cake", 0, 40, 74, "Greet your loved ones with a Happy Birthday with this package! Includes a dedication topper and a candle.", "birthday package");
-        spitem[10] = new SpcItem("Upgrade to Chocolate a la Mode", 0, 60, 136, "Ice Cream, Chocolate, and Cake? What a sweet combination! Inherits restrictions from Ice Cream and Chocolate.", "Ice Cream Chocolate Package", new SpcItem[]{spitem[6], spitem[7]});
-        spitem[11] = new SpcItem("Upgrade to Fruit a la Mode", 0, 60, 90, "Ice Cream, Fruit, and Cake? What a refreshing combination! Inherits restrictions from Ice Cream and Fruits.", "Ice Cream Fruit Package", new SpcItem[]{spitem[5], spitem[7]});
+        spitem[9] = new SpcItem("Upgrade to B-Day Cake", 0, 40, 74, "Greet your loved ones with a Happy Birthday with this package! Includes a dedication topper and a candle.", "birthday package", new SpcItem[]{spitem[0], spitem[3]});
+        spitem[10] = new SpcItem("Upgrade to Chocolate a la Mode", 0, 60, 136, "Ice Cream, Chocolate, and Cake? What a sweet combination! Inherits restrictions from Ice Cream and Chocolate.", "Ice Cream Chocolate Package", new SpcItem[]{spitem[7], spitem[8]});
+        spitem[11] = new SpcItem("Upgrade to Fruit a la Mode", 0, 60, 90, "Ice Cream, Fruit, and Cake? What a refreshing combination! Inherits restrictions from Ice Cream and Fruits.", "Ice Cream Fruit Package", new SpcItem[]{spitem[6], spitem[8]});
         for (int i = 12; i < spitem.length; i++) {
             spitem[i] = new SpcItem(); // Initialize the remaining elements with default SpcItem objects
         }
@@ -217,9 +217,9 @@ public class SpcStock {
      * @param intQuantity the amount of cakes to be added
      * 
      */
-        public void addNewSpcItem(String strName, String strDesc, int intCalorie, int intPrice, int intQuantity, String strTag){
+        public void addNewSpcItem(String strName, String strDesc, int intCalorie, int intPrice, int intQuantity, String strTag, String strFT){
             int index = countItems();
-            spitem[index] = new SpcItem(strName, intQuantity, intPrice, intCalorie, strDesc, strTag);
+            spitem[index] = new SpcItem(strName, intQuantity, intPrice, intCalorie, strDesc, strTag, strFT);
         }
     /**
      * Provides the index number of named items. Items set by default are still counted, even though they are deleted.
@@ -296,7 +296,14 @@ public class SpcStock {
         public String getItemName(int index) {
             return spitem[index].getName();
         }
-        
+    /**
+     * Gets the flavortext of the Item.
+     * @param index the index of the Item
+     * @return the flavortext  of the Item
+     */
+        public String getFT(int index) {
+            return spitem[index].getFlavorText();
+        }
 }
 
 
