028add94fb7cb2275b3dfae389107cb5d74d2adf
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2023-07-31 17:47:06 +0800

    Update SpcStock.java - Working well with spcGui

--- MCO1/SpcStock.java
+++ MCO1/SpcStock.java
@@ -11,7 +11,7 @@ public class SpcStock {
      * This is the constructor that initializes the SpcStock with default special items and their properties.
      */
     public SpcStock() {
-        spitem = new SpcItem[20];
+        spitem = new SpcItem[16];
         // Initialize default special items
         spitem[0] = new SpcItem("Happy Birthday Dedication", 0, 20, 74, "A special option for your cake, allowing you to greet your special person (or yourself) a Happy Birthday with this edible topper", "dedication");
         spitem[1] = new SpcItem("Happy Mothers Day Dedication", 0, 20, 74, "A special option for your cake, allowing you to greet your special person (or yourself) a Happy Mothers Day with this edible topper", "dedication");
@@ -21,23 +21,23 @@ public class SpcStock {
         spitem[4].addIncompatibleCake("Angel's Apple Crumble");
         spitem[5] = new SpcItem("Candy Hearts", 0, 40, 60, "A special option for your cake, allowing you to bring more love with these cute candied hearts. Not suitable for Crumbling cakes.", "topper");
         spitem[5].addIncompatibleCake("Angel's Apple Crumble");
-        spitem[5] = new SpcItem("Fruits", 0, 30, 24, "Add natural sweetness to your dessert with fruits! Tristan's Carrot Cake and Black Forest Cake cannot add fruit to the top of the cake.", "fruit");
-        spitem[5].addIncompatibleCake("Tristan's Carrot Cake");
-        spitem[5].addIncompatibleCake("Black Forest Cake");
-        spitem[6] = new SpcItem("Chocolate", 0, 40, 70, "Add a hint of bitter-sweet to your day with these chocolate toppings. Unavailable to Chocolate Mousse Cake,Black Forest Cake, and Premium Chocolate Cake as these are already chocolate cakes.", "chocolate");
-        spitem[6].addIncompatibleCake("Chocolate Mousse Cake");
+        spitem[6] = new SpcItem("Fruits", 0, 30, 24, "Add natural sweetness to your dessert with fruits! Tristan's Carrot Cake and Black Forest Cake cannot add fruit to the top of the cake.", "fruit");
+        spitem[6].addIncompatibleCake("Tristan's Carrot Cake");
         spitem[6].addIncompatibleCake("Black Forest Cake");
-        spitem[6].addIncompatibleCake("Premium Chocolate Cake");
-        spitem[7] = new SpcItem("Vanilla Ice Cream", 0, 30, 66, "Beat the heat by adding a scoop of fragrant and rich vanilla ice cream on your cake!Unavailable to Tristan's Carrot Cake, Black Forest Cake, Strawberry Cake, Chocolate Mousse Cake, and Lemon Blueberry Cake due to these cakes already have a delicate topping. ", "ice cream");
-        spitem[7].addIncompatibleCake("Tristan's Carrot Cake");
-        spitem[7].addIncompatibleCake("Black Forest Cake");
-        spitem[7].addIncompatibleCake("Strawberry Cake");
+        spitem[7] = new SpcItem("Chocolate", 0, 40, 70, "Add a hint of bitter-sweet to your day with these chocolate toppings. Unavailable to Chocolate Mousse Cake,Black Forest Cake, and Premium Chocolate Cake as these are already chocolate cakes.", "chocolate");
         spitem[7].addIncompatibleCake("Chocolate Mousse Cake");
-        spitem[7].addIncompatibleCake("Lemon Blueberry Cake");
-        spitem[8] = new SpcItem("Upgrade to B-Day Cake", 0, 40, 74, "Greet your loved ones with a Happy Birthday with this package! Includes a dedication topper and a candle.", "birthday package");
-        spitem[9] = new SpcItem("Upgrade to Chocolate a la Mode", 0, 60, 136, "Ice Cream, Chocolate, and Cake? What a sweet combination! Inherits restrictions from Ice Cream and Chocolate.", "Ice Cream Chocolate Package", new SpcItem[]{spitem[6], spitem[7]});
-        spitem[10] = new SpcItem("Upgrade to Fruit a la Mode", 0, 60, 90, "Ice Cream, Fruit, and Cake? What a refreshing combination! Inherits restrictions from Ice Cream and Fruits.", "Ice Cream Fruit Package", new SpcItem[]{spitem[5], spitem[7]});
-        for (int i = 11; i < spitem.length; i++) {
+        spitem[7].addIncompatibleCake("Black Forest Cake");
+        spitem[7].addIncompatibleCake("Premium Chocolate Cake");
+        spitem[8] = new SpcItem("Vanilla Ice Cream", 0, 30, 66, "Beat the heat by adding a scoop of fragrant and rich vanilla ice cream on your cake!Unavailable to Tristan's Carrot Cake, Black Forest Cake, Strawberry Cake, Chocolate Mousse Cake, and Lemon Blueberry Cake due to these cakes already have a delicate topping. ", "ice cream");
+        spitem[8].addIncompatibleCake("Tristan's Carrot Cake");
+        spitem[8].addIncompatibleCake("Black Forest Cake");
+        spitem[8].addIncompatibleCake("Strawberry Cake");
+        spitem[8].addIncompatibleCake("Chocolate Mousse Cake");
+        spitem[8].addIncompatibleCake("Lemon Blueberry Cake");
+        spitem[9] = new SpcItem("Upgrade to B-Day Cake", 0, 40, 74, "Greet your loved ones with a Happy Birthday with this package! Includes a dedication topper and a candle.", "birthday package");
+        spitem[10] = new SpcItem("Upgrade to Chocolate a la Mode", 0, 60, 136, "Ice Cream, Chocolate, and Cake? What a sweet combination! Inherits restrictions from Ice Cream and Chocolate.", "Ice Cream Chocolate Package", new SpcItem[]{spitem[6], spitem[7]});
+        spitem[11] = new SpcItem("Upgrade to Fruit a la Mode", 0, 60, 90, "Ice Cream, Fruit, and Cake? What a refreshing combination! Inherits restrictions from Ice Cream and Fruits.", "Ice Cream Fruit Package", new SpcItem[]{spitem[5], spitem[7]});
+        for (int i = 12; i < spitem.length; i++) {
             spitem[i] = new SpcItem(); // Initialize the remaining elements with default SpcItem objects
         }
     }
@@ -88,7 +88,24 @@ public class SpcStock {
             }
         }
     }
-
+    /**
+     * Displays item menu
+     * @param index of the item
+     * @return item details
+     */
+    public String displayDetails(int index){
+        if(!spitem[index].getName().equals(" ")){
+            return spitem[index].displayItem();
+        }
+        else{
+            return  "Name: " 
+            + "\nDescription: " 
+            + "\nCalorie Count: " 
+            + "\nPrice: " 
+            + "\nAvailable Quantity: ";
+        }
+        
+    }
     /**
      * Checks if a special item at the given index is valid (exists and has a quantity greater than 0).
      *
@@ -212,8 +229,8 @@ public class SpcStock {
 
         public int countItems() {
                 int intLoop;
-                int intCounter = 9;
-                for (intLoop = 9; intLoop < 20; intLoop++) {
+                int intCounter = 11;
+                for (intLoop = 11; intLoop < 20; intLoop++) {
                     if (!spitem[intLoop].getName().equals(" ")) {
                         intCounter = intCounter + 1;
                     }
